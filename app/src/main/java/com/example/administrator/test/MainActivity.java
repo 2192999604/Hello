f11e5bdb5730ea6a7af81e3f9aed62cfd4bcf77c
2192999604 <2192999604@qq.com>
2019-01-15 17:22:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.test;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = findViewById(R.layout.test);
+    }
+}
